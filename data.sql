d8c54e2c7c84b8138fb96a9d4113a0148c6c27b8
KisameReveal <kymiralouis.mallorca.cvt@eac.edu.ph>
2024-09-04 02:02:34 +0800

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,59 @@
+
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('1', 'rbowland0', 'Ruy', 'Bowland', '1998-03-19', '$2a$04$L0q559e1Q/yOwIjVoxS/W.6H5aLVFjpjlfI8qsfLSHnXqtcQID4wy', '2023-12-21');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('2', 'akretschmer1', 'Ag', 'Kretschmer', '1998-01-26', '$2a$04$4MN4Vh.b6wpO1wl4jZKfF.Y86J1Z4JSyelrOxNO4Fx5JoDXN9TkFW', '2024-03-12');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('3', 'aclabburn2', 'Angelique', 'Clabburn', '1993-02-12', '$2a$04$oSUkBKRdUEwM5wLzdhPYteuAcFgexxt7fqY2xBnLYGcS/X.1iwMVy', '2024-02-11');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('4', 'rsmithson3', 'Richmond', 'Smithson', '2003-03-24', '$2a$04$EJPSzLnkA44RJPfUBM9g8e.OMwrQWcgERjYOVqkVyTgsuqU4xdS86', '2024-03-05');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('5', 'stombleson4', 'Schuyler', 'Tombleson', '2004-10-07', '$2a$04$tULVel.md1ZR.CvpuM3CaeWSV4KJvhQP03wypoB28yW8R4LSNOhWq', '2024-01-01');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('6', 'mlefley5', 'Marleah', 'Lefley', '1999-07-13', '$2a$04$l4PHfB2iE2/Q0epmkM1RH.FydDJpRk8MGZfjvvjbkpm4jajI6Kq7a', '2023-09-04');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('7', 'rbalfe6', 'Rosaleen', 'Balfe', '1996-03-31', '$2a$04$nKjSrrNX1kwwJCcp3GRCOOxzfEEbJUQ4HHX.AT4VMr5KK9hW8TPCe', '2024-02-14');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('8', 'sinkster7', 'Susan', 'Inkster', '2002-12-10', '$2a$04$gVKOU6/t7Rc0Ze1y4jbjweBBp7OHLKABVCMCVJgcjFG8l1CaOB7R6', '2024-03-01');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('9', 'bcurran8', 'Brooks', 'Curran', '1998-01-02', '$2a$04$kxBNC4p6dFuGzp7TxeOk8.4CtgVs/o5KqtrL6sQMSOXFgmuPSq6aa', '2024-04-10');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) VALUES ('10', 'lhyett9', 'Leeann', 'Hyett', '2005-03-26', '$2a$04$Rxv7CiMDILHKPM362C5Jmu2MX3iEIpadZ1g00PPd4wffT2u0a1rKm', '2024-06-14');
+
+
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('1', '9', '5', 'false', '2023-10-24');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('2', '7', '6', 'true', '2023-12-14');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('3', '5', '4', 'true', '2023-09-11');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('4', '1', '10', 'true', '2024-06-11');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('5', '8', '7', 'false', '2023-12-05');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('6', '4', '8', 'false', '2023-11-05');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('7', '2', '2', 'true', '2024-04-29');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('8', '3', '3', 'false', '2024-05-24');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('9', '10', '1', 'true', '2023-12-09');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingadded, IsAccepted, DateAdded) VALUES ('10', '6', '9', 'true', '2023-11-26');
+
+
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('1', 'Shufflester', '8', '2024-01-28');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('2', 'Skimia', '7', '2024-08-12');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('3', 'Browsebug', '9', '2024-03-07');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('4', 'Digitube', '10', '2024-06-15');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('5', 'Yacero', '2', '2024-05-08');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('6', 'Mymm', '5', '2023-12-14');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('7', 'Realbuzz', '2', '2024-02-24');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('8', 'Quatz', '9', '2024-03-30');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('9', 'Babbleblab', '5', '2023-09-30');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded) VALUES ('10', 'Oyoloo', '10', '2023-09-13');
+
+
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('1', 'orchestrate turn-key web-readiness', '2', 'true', 'false', '1', '2023-10-04');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('2', 'drive sexy action-items', '3', 'true', 'true', '5', '2024-04-24');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('3', 'enhance front-end architectures', '3', 'true', 'true', '1', '2024-08-20');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('4', 'streamline global web services', '5', 'false', 'false', '2', '2024-03-11');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('5', 'implement viral initiatives', '9', 'true', 'true', '9', '2023-12-30');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('6', 'monetize plug-and-play e-commerce', '2', 'true', 'false', '8', '2024-08-19');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('7', 'unleash impactful mindshare', '8', 'true', 'true', '6', '2024-05-02');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('8', 'incubate rich metrics', '10', 'true', 'true', '1', '2023-11-17');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('9', 'streamline plug-and-play infomediaries', '9', 'false', 'true', '2', '2024-01-15');
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID, DatePosted) VALUES ('10', 'aggregate robust networks', '3', 'true', 'true', '5', '2024-05-29');
+
+
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('1', '2', '6', 'false', '2024-03-21');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('2', '1', '5', 'true', '2024-05-28');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('3', '8', '6', 'true', '2024-01-12');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('4', '6', '6', 'true', '2024-02-13');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('5', '3', '2', 'true', '2023-09-24');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('6', '5', '3', 'true', '2023-10-09');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('7', '6', '7', 'true', '2023-09-16');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('8', '10', '9', 'true', '2023-12-22');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('9', '3', '4', 'true', '2024-04-09');
+INSERT INTO GroupMemberShipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMembershipAccepted, DateAccepted) VALUES ('10', '7', '7', 'true', '2023-11-12');
